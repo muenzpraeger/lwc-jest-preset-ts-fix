5f2a28563055cbf674dc97f3b184bab0d4c849e2
muenzpraeger <mail@winkelmeyer.com>
2019-09-20 11:19:00 +0200

    Find TS tests

--- jest-preset.js
+++ jest-preset.js
@@ -5,23 +5,23 @@
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/MIT
  */
 module.exports = {
-    moduleFileExtensions: ['ts', 'js', 'html'],
-    moduleNameMapper: {
-        '^aura$': require.resolve('./src/stubs/aura.js'),
-        '^aura-instrumentation$': require.resolve('./src/stubs/auraInstrumentation.js'),
-        '^instrumentation-service$': require.resolve('./src/stubs/auraInstrumentation.js'),
-        '^aura-storage$': require.resolve('./src/stubs/auraStorage.js'),
-        '^lwc-test-utils$': require.resolve('@lwc/test-utils'),
-    },
-    testEnvironment: 'jest-environment-jsdom-fifteen',
-    resolver: require.resolve('@lwc/jest-resolver'),
-    transform: {
-        '^.+\\.(js|ts|html|css)$': require.resolve('@lwc/jest-transformer'),
-    },
-    snapshotSerializers: [require.resolve('@lwc/jest-serializer')],
-    testMatch: ['**/__tests__/**/?(*.)(spec|test).js'],
+  moduleFileExtensions: ["ts", "js", "html"],
+  moduleNameMapper: {
+    "^aura$": require.resolve("./src/stubs/aura.js"),
+    "^aura-instrumentation$": require.resolve("./src/stubs/auraInstrumentation.js"),
+    "^instrumentation-service$": require.resolve("./src/stubs/auraInstrumentation.js"),
+    "^aura-storage$": require.resolve("./src/stubs/auraStorage.js"),
+    "^lwc-test-utils$": require.resolve("@lwc/test-utils")
+  },
+  testEnvironment: "jest-environment-jsdom-fifteen",
+  resolver: require.resolve("@lwc/jest-resolver"),
+  transform: {
+    "^.+\\.(js|ts|html|css)$": require.resolve("@lwc/jest-transformer")
+  },
+  snapshotSerializers: [require.resolve("@lwc/jest-serializer")],
+  testMatch: ["**/__tests__/**/?(*.)(spec|test).{js,ts}"],
 
-    // temp workaround until this is released - https://github.com/facebook/jest/pull/6792
-    testURL: 'http://localhost/',
-    coveragePathIgnorePatterns: ['.css$', '.html$'],
+  // temp workaround until this is released - https://github.com/facebook/jest/pull/6792
+  testURL: "http://localhost/",
+  coveragePathIgnorePatterns: [".css$", ".html$"]
 };
